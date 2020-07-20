9b0e3de8ec0ceb3d5b9fe2da8bd63a93f4819198
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-07-20 06:33:35 -0500

    Create cals_2.py

---
+++ calls/cals_2.py
@@ -0,0 +1,30 @@
+from urllib.parse import urlencode
+
+# Import the Twilio Python Client.
+from twilio.rest import TwilioRestClient
+
+# Set your account ID and authentication token.
+account_sid = "your_twilio_account_sid"
+auth_token = "your_twilio_authentication_token"
+
+# The number of the phone initiating the call.
+# This should either be a Twilio number or a number that you've verified
+from_number = "+12028757564"
+
+# The number of the phone receiving call.
+to_number = "+527222692537"
+
+# Use the Twilio-provided site for the TwiML response.
+url = "https://twimlets.com/message?"
+
+# The phone message text.
+message = "Hello world."
+
+# Initialize the Twilio client.
+client = TwilioRestClient(account_sid, auth_token)
+
+# Make the call.
+call = client.calls.create(to=to_number,
+                           from_=from_number,
+                           url=url + urlencode({'Message': message}))
+print(call.sid)
